a4d7a454d052880456d7ddc53b5c4fbb74adbfdd
Cherishma <sha@biba.uni-bremen.de>
2022-01-20 10:57:35 +0100

    design

--- lib/main.dart
+++ lib/main.dart
@@ -37,44 +37,44 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  height: 10.0,
+                ),
+              ),
               Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(0.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal[800],
-                      ),
-                      title: Text(
-                        '+49 1573 0026730',
-                        style: TextStyle(
-                          color: Colors.teal.shade800,
-                          fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                  )),
-              Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.teal[800],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    '+49 1573 0026730',
+                    style: TextStyle(
+                      color: Colors.teal.shade800,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
-                    title: Text(
-                      'cherry.sh1996@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade800,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[800],
+                  ),
+                  title: Text(
+                    'cherry.sh1996@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade800,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
                   ),
                 ),
