777c7c57276d70921675cb2f059ad0adab2ce513
Cherishma <sha@biba.uni-bremen.de>
2022-01-20 10:46:30 +0100

    Card class added

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.teal[700],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
-                radius: 70.0,
+                radius: 80.0,
                 backgroundColor: Colors.white,
-                backgroundImage: AssetImage('images/profile_image.jpg'),
+                backgroundImage: AssetImage('images/356563.jpg'),
               ),
               Text(
                 'Cherishma Shankar',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
-                    fontSize: 30.0,
+                    fontSize: 35.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
@@ -31,9 +32,52 @@ class MyApp extends StatelessWidget {
                 'Android Developer'.toUpperCase(),
                 style: TextStyle(
                     fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
-                    letterSpacing: 2.5),
+                    fontSize: 18.0,
+                    color: Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal[800],
+                      ),
+                      title: Text(
+                        '+49 1573 0026730',
+                        style: TextStyle(
+                          color: Colors.teal.shade800,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  )),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(0.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[800],
+                    ),
+                    title: Text(
+                      'cherry.sh1996@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
